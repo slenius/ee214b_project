c4efcbc3c4695bf9dd366fc41ecf6e6d67d0ef5d
slenius <slenius@slenius-glaptop.roam.corp.google.com>
2016-03-02 21:52:48 -0800

    update

--- part_2/get_transistor_parameters.m
+++ part_2/get_transistor_parameters.m
@@ -4,12 +4,15 @@ m.wt = lookup(data, 'GM_CGG', 'GM_ID', m.gm_id, 'L', m.l);
 m.ft = m.wt  /(2 * pi);
 m.w = m.id / lookup(data, 'ID_W', 'GM_ID', m.gm_id, 'L', m.l);
 
-m.cgg = m.gm / lookup(data, 'GM_CGG', 'GM_ID', m.gm_id, 'L', m.l);
-m.cdd = m.gm / lookup(data, 'GM_CDD', 'GM_ID', m.gm_id, 'L', m.l);
-m.css = m.gm / lookup(data, 'GM_CSS', 'GM_ID', m.gm_id, 'L', m.l);
 m.vt = m.gm / lookup(data, 'GM_VT', 'GM_ID', m.gm_id, 'L', m.l);
 
 m.w = m.id / lookup(data, 'ID_W', 'GM_ID', m.gm_id, 'L', m.l);
+
+m.cgg = m.gm / m.wt;
+m.cgd = m.cgg * lookup(data, 'CGD_CGG', 'GM_ID', m.gm_id, 'L', m.l);
+m.cdd = m.cgg * lookup(data, 'CDD_CGG', 'GM_ID', m.gm_id, 'L', m.l);
+m.cdb = m.cdd - m.cgd;
+m.cgs = m.cgg - m.cgd;
 
 
 
