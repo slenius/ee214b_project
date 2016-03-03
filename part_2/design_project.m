de525e7fabeae8ed27ef8172dfe03603b4cfcbd0
Sanjana Singireddy <sanjana9@corn11.stanford.edu>
2016-03-02 21:54:44 -0800

    Mar_02

--- part_2/design_project.m
+++ part_2/design_project.m
@@ -1,8 +1,11 @@
 clear all
 close all
 
-load 180nch.mat
-load 180pch.mat
+addpath('../matlab');
+addpath('/usr/class/ee214b/matlab');
+
+load 180nch.mat;
+load 180pch.mat;
 
 design = struct();
 constraints = ee214b_defaults('mos180', 300);
@@ -12,18 +15,20 @@ constraints.overall_gain_cl = 720;
 constraints.total_current = 10.1e-3;
 constraints.input_noise = 20e-12;
 constraints.cl = 20e-15;
+constraints.gamma = 0.85;
+constraints.cd = 200e-15;
+constraints.input_noise_dist = [1/3 4/9 1/9];
+constraints.noise = constraints.input_noise_dist .* (constraints.input_noise^2);
+
 
 constraints.gm_id1 = 15;
 constraints.gm_id2 = 15;
 constraints.gm_id3 = 15;
 
-%design = get_gm_rf_from_noise(design, constraints);
+design = get_gm_rf_from_noise(design, constraints);
 
-design.r_f = 700;
-
-design = get_af_from_rf_and_acl(design, constraints);
-
+%design = get_af_from_rf_and_acl(design, constraints);
+%design = get_time_constants(design, constraints); 
 
 
-d = design
 
--- part_2/get_gm_rf_from_noise.m
+++ part_2/get_gm_rf_from_noise.m
@@ -1,6 +1,11 @@
-function design = get_gm_rf_from_noise(design, constraints)
-    fprintf('test = %f\n', constraints.vdd);
-
+function  d = get_gm_rf_from_noise(d, constraints )
+%Distributes noise equally among all three components
+    d.noise.r_f  = constraints.noise(1) ;
+    d.noise.m1   = constraints.noise(2) ;
+    d.noise.mb1  = constraints.noise(3) ;
+    d.r_f = 4*constraints.k*constraints.T/d.noise.r_f ;
+    d.m1.gm = d.noise.m1/(4*constraints.k*constraints.T*constraints.gamma) ;
+    d.mb1.gm = d.noise.mb1/(4*constraints.k*constraints.T*constraints.gamma) ;
     
-    design.m1.gm = 1.23;
 end
+
