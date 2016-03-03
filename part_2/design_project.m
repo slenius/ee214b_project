1646ae5e8748ac73ec158f8551cb72785d36e91a
slenius <slenius@slenius-glaptop.roam.corp.google.com>
2016-03-02 20:27:58 -0800

    adding ee214b defaults

--- part_2/design_project.m
+++ part_2/design_project.m
@@ -1,12 +1,14 @@
 clear all
 close all
 
+load 180nch.mat
+load 180pch.mat
 
 design = struct();
-constraints = struct();
+constraints = ee214b_defaults('mos180');
 
 constraints.vdd = 1.8;
-constraints.overal_gain_cl = 720;
+constraints.overall_gain_cl = 720;
 constraints.total_current = 10.1e-3;
 constraints.input_noise = 20e-12;
 constraints.cl = 20e-15;
@@ -15,8 +17,13 @@ constraints.gm_id1 = 15;
 constraints.gm_id2 = 15;
 constraints.gm_id3 = 15;
 
-design = get_gm_rf_from_noise(design, constraints);
+%design = get_gm_rf_from_noise(design, constraints);
+
+design.r_f = 700;
+
+design = get_af_from_rf_and_acl(design, constraints);
 
 
 
+d = design
 
