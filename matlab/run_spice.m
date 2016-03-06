c77a7d73db50b771750cda25723e72510d6ee05e
slenius <slenius@slenius-glaptop.roam.corp.google.com>
2016-03-06 11:05:35 -0800

    modified run_spice to run on corn

--- matlab/run_spice.m
+++ matlab/run_spice.m
@@ -1,5 +1,18 @@
 function run_spice()
 
-system('export LM_LICENSE_FILE="31000@license.corp.google.com"; /usr/local/google/edatools/synopsys/hspice/H-2013.03/hspice/bin/hspice matlab.sp');
-    
+hostname = char(getHostName(java.net.InetAddress.getLocalHost));
+if findstr('google', hostname)
+    lic = 'export LM_LICENSE_FILE="31000@license.corp.google.com"; ';
+    hspice = '/usr/local/google/edatools/synopsys/hspice/H-2013.03/hspice/bin/hspice ';
+else
+    lic = 'export LM_LICENSE_FILE="27000@cadlic0"; ';
+    hspice = '/usr/class/ee/synopsys/hspice/I-2013.12-SP2/hspice/ ';
+end
+
+spicefile = 'matlab.sp';
+
+cmd = [lic hspice spicefile];
+
+system(cmd);
+
 end
